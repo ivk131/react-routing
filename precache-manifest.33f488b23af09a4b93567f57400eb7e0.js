1a657faf6a0390a8ae90e857a75f64308c13fed1
Vikash Kumar <vikashsmart1996@gmail.com>
2020-09-14 13:46:14 +0530

    Updates

---
+++ precache-manifest.33f488b23af09a4b93567f57400eb7e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2088ed759425bc1462177d1af2919cab",
+    "url": "/react-routing/index.html"
+  },
+  {
+    "revision": "8e50460a6ba746c2bb74",
+    "url": "/react-routing/static/css/main.05139830.chunk.css"
+  },
+  {
+    "revision": "ef7fed73be404c58c8bf",
+    "url": "/react-routing/static/js/2.95b6e8bb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/react-routing/static/js/2.95b6e8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e50460a6ba746c2bb74",
+    "url": "/react-routing/static/js/main.d6419226.chunk.js"
+  },
+  {
+    "revision": "febc0792d0b8238670a9",
+    "url": "/react-routing/static/js/runtime-main.1d86498f.js"
+  },
+  {
+    "revision": "25cf1a1f4bcdc2b3e18afb6155927051",
+    "url": "/react-routing/static/media/company_logo.25cf1a1f.png"
+  },
+  {
+    "revision": "0f01b70f6624bb8829a93235de4ae41d",
+    "url": "/react-routing/static/media/intensel.0f01b70f.png"
+  }
+]);
